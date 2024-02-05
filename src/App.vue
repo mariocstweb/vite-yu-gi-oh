656e5a65073da092fa84be91bb588e5912610d69
mariocstweb <mariocstmra@gmail.com>
2024-02-05 16:44:37 +0100

    added pokemon.card component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <script>
 import AppHeader from "./components/AppHeader.vue"
 import AppMain from "./components/AppMain.vue";
+import axios from "axios";
+import { store } from "./data/store.js"
+const endpoint = "https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons";
 export default {
-  components: {
-    AppHeader, AppMain
+  name: "Pokedex",
+
+  components: { AppHeader, AppMain },
+  created() {
+    axios.get(endpoint).then(res => {
+      store.pokemons = res.data.results;
+    })
+    console.log(store);
   }
 }
 </script>
--- src/components/PokemonCard.vue
+++ src/components/PokemonCard.vue
@@ -1,12 +1,14 @@
 <script>
+import { store } from "../../src/data/store.js"
 export default {
   name: "PokemonCard",
+  data: () => ({ store })
 }
 </script>
 
 <template>
   <div class="card">
-    <img src="..." class="card-img-top" alt="...">
+    <img :src="store.imageUrl" class="card-img-top" alt="...">
     <div class="card-body">
       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
         content.</p>
