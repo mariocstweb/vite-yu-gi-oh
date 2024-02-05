772239bdbeb14056e5f7fed6c97af092f6189ae7
mariocstweb <mariocstmra@gmail.com>
2024-02-05 15:55:54 +0100

    added component

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,9 +1,28 @@
 <script>
+import PokemonCard from "./PokemonCard.vue";
 export default {
   name: "AppMain",
+  components: { PokemonCard }
 }
+
 </script>
 
-<template></template>
+<template>
+  <div class="container bg-danger">
+    <div class="row row-cols-5 p-5 d-flex justify-content-between flex-wrap g-5">
+      <!-- COL -->
+      <div class="col p-2">
+        <PokemonCard />
+      </div>
+      <!-- COL -->
+    </div>
+  </div>
+</template>
 
-<style scoped></style>
+<style scoped>
+.row {
+  margin-top: 100px;
+  background-color: #DEDEDE;
+  border-radius: 30px;
+}
+</style>
